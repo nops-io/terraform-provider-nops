cba1b939687e6a6879e88b08982021100cc0fef2
Werner Matias Kahnlein <matias.kahnlein@caylent.com>
2025-01-06 18:14:37 -0300

    fix: remove crud until fix in ken

--- nops/nops_container_cost_bucket.go
+++ nops/nops_container_cost_bucket.go
@@ -3,10 +3,8 @@ package nops
 import (
 	"context"
 	"fmt"
-	"strconv"
 	"time"
 
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -25,12 +23,8 @@ type containerCostBucketResource struct {
 }
 
 type containerCostBucketModel struct {
-	ID          types.Int64  `tfsdk:"id"`
 	LastUpdated types.String `tfsdk:"last_updated"`
 	ProjectId   types.Int64  `tfsdk:"project_id"`
-	Status      types.String `tfsdk:"status"`
-	Region      types.String `tfsdk:"region"`
-	Bucket      types.String `tfsdk:"bucket"`
 }
 
 // computeCopilotResource is a helper function to simplify the provider implementation.
@@ -71,10 +65,6 @@ func (r *containerCostBucketResource) Schema(_ context.Context, _ resource.Schem
 		Description: "Notifies the nOps platform a new container cost bucket was created for the backend to fetch metadata from it." +
 			" This resource is mostly used only for secure connection with nOps APIs.",
 		Attributes: map[string]schema.Attribute{
-			"id": schema.Int64Attribute{
-				Computed:    true,
-				Description: "Timestamp when the resource was last updated.",
-			},
 			"project_id": schema.Int64Attribute{
 				Required:    true,
 				Description: "nOps project ID.",
@@ -82,18 +72,6 @@ func (r *containerCostBucketResource) Schema(_ context.Context, _ resource.Schem
 			"last_updated": schema.StringAttribute{
 				Computed:    true,
 				Description: "Timestamp when the resource was last updated.",
-			},
-			"bucket": schema.StringAttribute{
-				Computed:    true,
-				Description: "AWS bucket name associate with this integration.",
-			},
-			"region": schema.StringAttribute{
-				Computed:    true,
-				Description: "AWS region where the bucket resides.",
-			},
-			"status": schema.StringAttribute{
-				Computed:    true,
-				Description: "nOps Container Cost Bucket integration status.",
 			},
 		},
 	}
@@ -120,24 +98,6 @@ func (r *containerCostBucketResource) Create(ctx context.Context, req resource.C
 		return
 	}
 
-	allContainerBuckets, err := r.client.GetContainerCostBucketSetupStatus()
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Error getting remote compute copilot onboarding data",
-			err.Error(),
-		)
-		return
-	}
-
-	for _, integration := range *allContainerBuckets {
-		if plan.ProjectId.ValueInt64() == integration.Project {
-			plan.ID = types.Int64Value(integration.ID)
-			plan.Bucket = types.StringValue(integration.Bucket)
-			plan.Region = types.StringValue(integration.Region)
-			plan.Status = types.StringValue(integration.Status)
-		}
-	}
-
 	// Set state to fully populated data
 	plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
 	diags = resp.State.Set(ctx, plan)
@@ -158,22 +118,6 @@ func (r *containerCostBucketResource) Read(ctx context.Context, req resource.Rea
 		return
 	}
 
-	containerCostBucketStatus, err := r.client.GetTargetedContainerCostBucketSetupStatus(state.ID.ValueInt64())
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Error getting remote compute copilot onboarding data",
-			err.Error(),
-		)
-		return
-	}
-
-	state.ID = types.Int64Value(containerCostBucketStatus.ID)
-	state.Bucket = types.StringValue(containerCostBucketStatus.Bucket)
-	state.Region = types.StringValue(containerCostBucketStatus.Region)
-	state.Status = types.StringValue(containerCostBucketStatus.Status)
-
-	tflog.Debug(ctx, "Upstream container cost bucket data received for project "+strconv.Itoa(int(state.ProjectId.ValueInt64())))
-
 	// Set refreshed state
 	diags = resp.State.Set(ctx, &state)
 	resp.Diagnostics.Append(diags...)
@@ -189,37 +133,6 @@ func (r *containerCostBucketResource) Update(ctx context.Context, req resource.U
 	resp.Diagnostics.Append(diags...)
 	if resp.Diagnostics.HasError() {
 		return
-	}
-
-	var containerCostBucket ContainerCostBucketSetup
-	containerCostBucket.Project = plan.ProjectId.ValueInt64()
-
-	err := r.client.NotifyContainerCostBucketSetup(containerCostBucket)
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Error notifying nOps",
-			"Failed to notify, unexpected error: "+err.Error(),
-		)
-		return
-	}
-
-	allContainerBuckets, err := r.client.GetContainerCostBucketSetupStatus()
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Error getting remote compute copilot onboarding data",
-			err.Error(),
-		)
-		return
-	}
-
-	for _, integration := range *allContainerBuckets {
-		if plan.ProjectId.ValueInt64() == integration.Project {
-			plan.ID = types.Int64Value(integration.ID)
-			plan.Bucket = types.StringValue(integration.Bucket)
-			plan.Region = types.StringValue(integration.Region)
-			plan.Status = types.StringValue(integration.Status)
-
-		}
 	}
 
 	// Set state to fully populated data
@@ -241,22 +154,4 @@ func (r *containerCostBucketResource) Delete(ctx context.Context, req resource.D
 	if resp.Diagnostics.HasError() {
 		return
 	}
-
-	err := r.client.DeleteContainerCostBucket(state.ID.ValueInt64())
-	if err != nil {
-		resp.Diagnostics.AddError(
-			"Error deleting container cost bucket",
-			err.Error(),
-		)
-		return
-	}
-}
-
-func (r *containerCostBucketResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
-	// Capability to import existing cc onboarding already integrated in the nOps platform into the TF state without recreation.
-	val, err := strconv.Atoi(req.ID)
-	if err != nil {
-		resp.Diagnostics.AddError("Error parsing ID for import, please check for a correct project ID", err.Error())
-	}
-	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), val)...)
 }
